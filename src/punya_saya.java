9d0ae6a005a5a9c712d078bda97c43ba81c40f81
Lenovo <saputrayahya0607@gmail.com>
2023-11-13 06:21:47 +0700

    punya-saya

--- src/punya_saya.java
+++ src/punya_saya.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class punya_saya {
+public class punya_saya {
+    public static void main(String[] args) {
+//        for (int x =1;x < 15 ; x++){
+//        System.out.println(x);}
+        String status;
+        for (int angka = 10; angka < 20; angka++){
+            if (angka % 2 == 0){
+                status = "angka genap";
+            }else{
+                status = "angka ganjil";
+            }
+            System.out.println(angka + " adalah " + status);
+        }
+    }
 }
